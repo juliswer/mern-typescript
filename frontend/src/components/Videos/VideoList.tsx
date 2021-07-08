0704cf6a82f6ef04130ff0f433886d81811b00b2
Julián Swerdlin <julianswer36@gmail.com>
2021-07-08 12:08:53 -0300

    uploading new files

--- frontend/src/components/Videos/VideoList.tsx
+++ frontend/src/components/Videos/VideoList.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
+import {Video} from './Video';
 
 const VideoList = () => {
+
+    const [videos, setVideos] = useState<Video[]>([]);
+
+    const loadVideos = async () => {
+        const res = await axios.get('http://localhost:4000/videos')
+        setVideos(res.data)
+    }
+ 
+    useEffect(() => {
+        loadVideos()
+        return () => {
+            
+        };
+    }, []);
+
     return (
         <div>
-            Video List
+            {videos.map((video) => {
+                return (
+                <div>
+                    <h1>{video.title}</h1>
+                </div>
+                )
+            })}
         </div>
     )
 }
