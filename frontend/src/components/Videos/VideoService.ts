908bc9b9d72de10a018ae286c057078826b60277
Julián Swerdlin <julianswer36@gmail.com>
2021-07-10 17:03:21 -0300

    uploading and updating files

--- frontend/src/components/Videos/VideoService.ts
+++ frontend/src/components/Videos/VideoService.ts
@@ -13,13 +13,13 @@ export const createVideo = async (video: Video) => {
 }
 
 export const getVideo = async (id: string) => {
-    return await axios.get<Video>(`${API}/videos/${id}`)
+    return await axios.get<Video>(`${API}/videos/${id}`);
 }
 
 export const updateVideo = async (id: string, video: Video) => {
-    return await axios.put<Video>(`${API}/videos/${id}`, video)
+    return await axios.put<Video>(`${API}/videos/${id}`, video);
 }
 
 export const deleteVideo = async (id: string) => {
-    return await axios.delete<Video>(`${API}/videos/${id}`)
+    return await axios.delete<Video>(`${API}/videos/${id}`);
 }
